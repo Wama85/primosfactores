b41ac9552453ffa388fd02fb2b18b6e4eea6e9de
wilnermena <wi_an9@hotmail.com>
2023-09-24 18:47:24 -0400

    Debería retornar una lista [2]

--- src/factoresprimos.test.js
+++ src/factoresprimos.test.js
@@ -5,10 +5,19 @@ describe("FACTORES PRIMOS", () => {
     const primosfac= new primos();
     expect(primosfac.factorprimo(1)).toEqual([]);
   });
+  it("Debería retornar una lista  [2]", () => {
+    const primosfac= new primos();
+    expect(primosfac.factorprimo(2)).toEqual([2]);
+  });
+
 });
 
 class primos{
-  factorprimo(){
-    return [];
+  factorprimo(n){
+    let resulprimos=[];
+    if (n>1){
+      resulprimos.push(2);
+    } 
+    return resulprimos;
   }
 }
